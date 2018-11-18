ff01f880804306b43d8d7800952ab2592dfe96d8
Jafeth Bendezu <claro_pool_db@gmail.com>
2018-11-17 21:21:04 -0500

    begin

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<style>
+    html, body {
+        margin: 0px;
+        padding: 0px;
+        border: 0px;
+        width: 100%;
+        height: 100%;
+    }
+    iframe {
+        width: 100%;
+        height: 2400px;
+        margin: 0px;
+        padding: 0px;
+        border: 0px;
+        display: block;
+    }
+    .holder{
+        width: 100%;
+        height:2400px;
+        position:relative;
+    }
+    .header{
+        position:absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height:40px;
+        background-color: white;
+    }
+    .footer{
+        position:absolute;
+        bottom:0;
+        width:100%;
+        height:40px;
+        background-color: white;
+    }
+</style>
+</head>
+
+
+<body>
+<div class="holder">
+<iframe src="https://cuchinc.wixsite.com/la88" frameborder="0" scrolling="no"></iframe>
+<div class="header"></div>
+<div class="footer"></div>
+</div><200b>
+</body>
+</html>
+
